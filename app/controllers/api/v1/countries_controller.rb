bbb86dddddc3283a9764b66b66eaa574d17daaf5
Miroslav Ivanov <mivanov@scholastic.co.uk>
2018-06-19 12:22:58 +0100

    - fix module namespacing

--- app/controllers/api/v1/countries_controller.rb
+++ app/controllers/api/v1/countries_controller.rb
@@ -1,8 +1,25 @@
-module Api::V1
-  class CountriesController < ApplicationController
-    def show
-      @countries = Country.all
-      render json: @countries
+module Api
+  module V1
+    class CountriesController < ApplicationController
+      def show
+        @countries = Country.order('country ASC')
+        render json: @countries
+      end
+
+      def create
+        @countries = Country.create(country_params)
+        render json: @countries
+      end
+
+      def destroy
+        @countries = Country.find(params[:id])
+      end
+
+      private
+
+      def country_params
+        params.require(:country).permit(:country, :capital)
+      end
     end
   end
 end
